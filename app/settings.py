637e2230e00c22f0fbeb0c6951e665fe3378af37
Daniel Kamar <daniel@koitoror.ml>
2023-05-18 23:53:18 +0000

    bug-fix: deploy api railway

--- app/settings.py
+++ app/settings.py
@@ -136,8 +136,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'staticfiles'), os.path.join(BASE_DIR, 'static/admin')]
+
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

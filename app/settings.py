e1ef7e576ef6edadc62de09de9fd1368d768e920
Daniel Kamar <daniel@koitoror.ml>
2023-05-19 00:13:05 +0000

    bug-fix: deploy api railway

--- app/settings.py
+++ app/settings.py
@@ -146,7 +146,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 

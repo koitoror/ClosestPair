e00de52643305336e4f349a0366b87a29154496b
Daniel Kamar <kamarster@gmail.com>
2023-05-20 22:26:55 +0300

    bug-fix: tests reverse error

--- app/urls.py
+++ app/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('closest_pair_api/', PointsAPIView.as_view(), name='points-api'),
     # path('closest_pair_api/', PointsAPIView.as_view(), ),
-    path('/', include('api.urls', name='points-api'))
+    path('/', include('api.urls', namespace='points-api'))
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

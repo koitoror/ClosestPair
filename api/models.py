e4e0c946f0bec0c6abe2793e58e5bef131f8c868
Daniel Kamar <kamarster@gmail.com>
2023-05-20 10:49:08 +0300

    bug-fix: deploy migrations

--- api/models.py
+++ api/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Points(models.Model):
     points = models.CharField(max_length=200)
-    closest_pair = models.CharField(max_length=200)
+    closest_pairs = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)

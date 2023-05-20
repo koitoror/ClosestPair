74131f87959ac144cf2c9efadc6a2f788b0a42a3
Daniel Kamar <kamarster@gmail.com>
2023-05-20 10:31:37 +0300

    bug-fix: deploy migrations

--- release.sh
+++ release.sh
@@ -24,10 +24,11 @@ echo "Running Release Tasks"
 # python manage.py collectstatic --noinput
 
 echo "Running Database migrations and migrating the new changes"
-python manage.py makemigrations
+python manage.py makemigrations 
+python manage.py makemigrations api
 
-python manage.py migrate --plan
-python manage.py migrate --noinput
+# python manage.py migrate --plan
+# python manage.py migrate --noinput
 python manage.py migrate api
 
 echo "Running Server"
